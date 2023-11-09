a2650793fe4b5bd2b7b92ff64647a7722e8a85c9
mohamad <mohamad.alibrahim1998@gmail.com>
2023-11-09 09:27:26 +0200

    Displaying  score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Game } from "../hooks/useGames";
-import { Card, CardBody, Heading, Image, Te } from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image, Te } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
 interface Props {
   game: Game;
 }
@@ -11,7 +12,11 @@ const GameCard = ({ game }: Props) => {
       <Image src={game.background_image} />
       <CardBody borderRadius={10}>
         <Heading>{game.name}</Heading>
-       <PlatformIconList platform={game.parent_platforms.map(p=>p.platform)}/>
+        <HStack justifyContent={'space-between'}>
+        <PlatformIconList platform={game.parent_platforms.map(p=>p.platform)}/>
+       <CriticScore score={game.metacritic} />
+        </HStack>
+     
       </CardBody>
     </Card>
   );
